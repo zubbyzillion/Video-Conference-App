ade5fcd8770f58e265e642647a6e2256fe39e337
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-12-02 23:43:56 +0100

    Setting up config files

---
+++ script.js
@@ -0,0 +1,13 @@
+let client = AgoraRTC.createClient(
+    {
+        mode: "rtc",
+        'codec':"vp8"
+    }
+)
+
+let config = {
+    appid: "264655385f5c48549bae0190c6b6bc2b",
+    token: "007eJxTYDiaEfKnKLjXd9v87aF5n7I15m1SDNS9vPas5EuWduupFwQUGIzMTMxMTY0tTNNMk00sTE0skxJTDQwtDZLNksySko2S+DdlpzYEMjIcOvGRgREKQXxuhqrMnJzM/DyFzLxkBgYAUNoiqA==",
+    uid: null,
+    channel: "zillion inc",
+}
