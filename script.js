0ec8f8891dd7db253368a2868c9c3f504e9ac3e5
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-12-03 19:13:41 +0100

    async function to connect videoApi

--- script.js
+++ script.js
@@ -11,3 +11,34 @@ let config = {
     uid: null,
     channel: "zillion inc",
 }
+
+let localTracks = {
+    audioTracks:null,
+    videoTracks:null,
+}
+
+let remoteTracks = {}
+
+document.getElementById("join-btn").addEventListener("click", async ()=> {
+    console.log("User Joined stream")
+    await joinStreams()
+})
+
+let joinStreams = async () => {
+
+    [config.uid, localTracks.audioTracks, localTracks] = await Promise.all([
+        client.join(config.appid, config.channel, config.token),
+        AgoraRTC.createMicrophoneAudioTrack(),
+        AgoraRTC.createCameraVideoTrack(),
+    ])
+
+    let videoPlayer = `<div class="video-containers" id="video-wrapper-${config.uid}" >
+        <p class="user-uid" >${config.uid}</p>
+        <div class="video-player player" id="stream-${config.uid}" ></div>
+    </div>`
+
+    document.getElementById('user-streams').insertAdjacentElement('beforeend', videoPlayer)
+    localTracks.videoTracks.play(`stream-${config.uid}`)
+
+    await client.publish([localTracks.audioTracks, localTracks.videoTracks])
+}
